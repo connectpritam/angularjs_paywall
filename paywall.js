e0a6b55987785e12576ca7e7edc27c34a307db44
Pritam Mullick <pmullick@wrench.com>
2021-04-26 00:54:02 +0530

    angularjs boilerplate code

---
+++ paywall.js
@@ -0,0 +1,11 @@
+// module _p
+var _p = angular.module('paywall', [
+    // our dependencies will be injected here
+]);
+
+// controllers
+_p.controller('sampleController', function ($scope) {
+    $scope.name = 'Arijit';
+});
+
+// controller-functions
