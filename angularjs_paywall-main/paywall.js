ec6a416007222e20c8c05847c25b9f514887e450
Ghosh <AGhosh56@its.jnj.com>
2021-06-06 23:23:06 +0530

    sign in and sign up page

--- angularjs_paywall-main/paywall.js
+++ angularjs_paywall-main/paywall.js
@@ -4,76 +4,106 @@ var _p = angular.module('paywall', [
 ]);
 
 // controllers
-_p.controller('sampleController', function ($scope) {
-    $scope.isSuccess=false
-    $scope.passSucess=false
-    $scope.passStrengthSucess=false
+_p.controller('sampleController', function ($scope,$window,$sce,$location) {
+    $scope.isSuccess=false  
     $scope.phoneSucess=false
     $scope.emailSucess=false
-   $scope.checkPass= function (){
-       var d1=document.getElementById('pwd_mismatch')
-        if($scope.pass!==$scope.confirmpass){
-            d1.style.display="block"
-        }
-        else{
-            d1.style.display="none"
-            $scope.passSucess=true
-        }
-      }
-    $scope.check_pass_strength=function(){
-        var d1=document.getElementById('pass_mismatch')
-        if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test($scope.pass)){
-            d1.style.display="none"
-            $scope.passStrengthSucess=true
-        }
-        else{
-            d1.style.display="block"
-        }
-    }
+
       $scope.emailCheck =function(){
-        var d1=document.getElementById('email_mismatch')
+       
             if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($scope.email)){
-                d1.style.display="none"
+            
+                $("#email_mismatch").hide()
                 $scope.emailSucess=true
             }
             else{
-                d1.style.display="block"
+                $("#email_mismatch").show()
+                
             }
         }
+     
       $scope.checkPhone= function (){
-        var d1=document.getElementById('phone_mismatch')
        
          if(/^[0-9]{10,10}$/.test($scope.phone)){
-             d1.style.display="none"
+            $("#phone_mismatch").hide()
              $scope.phoneSucess=true
          }
          else{  
-             d1.style.display="block"
+            $("#phone_mismatch").show()
          }
        }
-       
-       console.log($scope.isSuccess)
-       const form = document.getElementById('signupForm');
-       form.addEventListener('submit', e => {
-        e.preventDefault();
-        $scope.isSuccess= $scope.phoneSucess &&  $scope.passSucess &&  $scope.passStrengthSucess &&  $scope.emailSucess
-        if($scope.isSuccess){
-            console.log($scope.phone,$scope.pass,$scope.email)
-        }
-        else{
-            console.log('err')
-        }
-    });
+
+     $scope.loginCheck=  function(){
+        //e.preventDefault()
+        var isAccountPresent=false
+        var isSubscriptionDone=true
+  if(isAccountPresent && isSubscriptionDone){
+      $(".mainwrapper").css({"opacity":"1"})
+      $("#overlay").fadeOut(500)
+  }
+  if(isAccountPresent && !isSubscriptionDone){
+    $(".login").load('subscription.html')
+  }
+  else{
+    $window.location.href = '/signUp.html';
+    //$location.url('/SignUp.html')
+    $("#name").val(localStorage.getItem('name'))
+    $("#email").val(localStorage.getItem('email'))
+  
+   };
+      
+  }
+  
+  /*
+  const form = document.getElementById('signupForm');
+  if(form!==undefined){
+  form.addEventListener('submit', e => {
+   e.preventDefault();
+   $scope.isSuccess= $scope.phoneSucess &&  $scope.emailSucess
+   if($scope.isSuccess){
+       console.log($scope.phone,$scope.email)
+   }
+   else{
+       console.log('err')
+   }
+})
+}
+*/
+
+
 });
+
+
 gapi.load('auth2', function(){
     gapi.auth2.init()
 })
+
+
+$( document ).ready(function() {
+    //var signedIn=localStorage.getItem('signedIn')
+    var signedIn= 'false'
+    if(signedIn==='false'){
+        $(".mainwrapper").css({"cursor":"none","opacity":"0.2","background":"blue"})
+    }   
+   
+});
+
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    $("#uname").val(profile.getEmail())
+    localStorage.setItem('name',profile.getName())
+    localStorage.setItem('email',profile.getEmail())
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail()); 
+    // This is null if the 'email' scope is not present.
+    
   }
+ 
+    
+  
+
  
 // controller-functions
