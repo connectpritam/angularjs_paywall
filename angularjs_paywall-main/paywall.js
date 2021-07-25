9cd08294f39ca3944270aae60106c03891857dc3
Ghosh <AGhosh56@its.jnj.com>
2021-07-25 13:56:31 +0530

    sign in and sign up page

--- angularjs_paywall-main/paywall.js
+++ angularjs_paywall-main/paywall.js
@@ -17,7 +17,6 @@ app.config(function ($routeProvider, $locationProvider)
                    $location.path('/home/')
                 }
                 else{
-                    console.log('2')
                     $location.path('/signIn/')
                 }
                
@@ -59,7 +58,7 @@ app.config(function ($routeProvider, $locationProvider)
 
 
 // controllers
-app.controller('signUpController', function ($scope,$rootScope,$location,$http) {
+app.controller('signUpController', function ($scope,$rootScope,$location,$http,$httpParamSerializerJQLike) {
     $scope.isSuccess=false  
     $scope.phoneSucess=false
     $scope.emailSucess=false
@@ -69,8 +68,6 @@ app.controller('signUpController', function ($scope,$rootScope,$location,$http) 
        
     };
     
-     
-   
     if(localStorage.getItem('name')||localStorage.getItem('email')){
 
         $("#name").val(localStorage.getItem('name'))
@@ -82,7 +79,9 @@ app.controller('signUpController', function ($scope,$rootScope,$location,$http) 
             if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($scope.email)){
                 
                 $("#email_mismatch").hide()
+                $('#phone_present').hide()
                 $scope.emailSucess=true
+                $('#signUp').attr('disabled',false)
             }
             else{
                 $("#email_mismatch").show()  
@@ -93,6 +92,8 @@ app.controller('signUpController', function ($scope,$rootScope,$location,$http) 
          if(/^[0-9]{10,10}$/.test($scope.phone)){
             $("#phone_mismatch").hide()
              $scope.phoneSucess=true
+             $('#phone_present').hide()
+             $('#signUp').attr('disabled',false)
          }
          else{  
             $("#phone_mismatch").show()
@@ -106,8 +107,50 @@ app.controller('signUpController', function ($scope,$rootScope,$location,$http) 
         //on success ajax call to server to post form contents
 
         //route to subscription page
+        $('.loader').show()
+        $('#signUp').attr('disabled',true)
+        $('#phone_present').hide()
         if( $scope.phoneSucess&& $scope.emailSucess){
-            $location.path('/subscription')
+           
+           /*
+            $http({
+                url: 'https://uttarbangasambad.in/api/users/register.php',
+                method: "POST",
+                data:$.param({
+                   
+                    
+                }),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            })
+            */
+            var configs = { cache: false };
+            var payload = {
+                name: $("#name").val(),
+                email: $scope.email, 
+                phone: $scope.phone
+            };
+      
+      // The Method Post is generally used with a payload,
+      configs.params = payload;
+            $http.post('https://uttarbangasambad.in/api/users/register.php', null /* but normally payload */, configs)
+            .then(function successCallback(response) {
+                console.log(response)
+                  if( response.status==200){
+                    $('#signUp').attr('disabled',false)
+                    $rootScope.isAccountPresent=true
+                    $location.path('/subscription/')
+                  }
+                  else {
+                    console.log(response.status)
+                    $('#phone_present').show()
+                    $('.loader').hide()
+                }
+                }, function errorCallback(response) {
+                 alert('there was a problem in signing up.please try again')
+                 $('.loader').hide()
+                 $('#signUp').attr('disabled',false)
+                });
+           
         }
         else{
             $('#signUp').attr('disabled',true)
@@ -122,6 +165,7 @@ app.controller('signInController',function ($scope,$rootScope,$location,$http){
     $scope.init=function() {
         //var signedIn=localStorage.getItem('signedIn')
         //$rootScope.isAccountPresent=false
+        
         if(!$rootScope.isAccountPresent){
             $(".mainwrapper").css({"cursor":"none","opacity":"0.2","background":"blue"})
         } 
@@ -137,11 +181,26 @@ app.controller('signInController',function ($scope,$rootScope,$location,$http){
     $scope.loginCheck=function(){  
         $rootScope.isSubscritonpresent=true
         $rootScope.isAccountPresent=false
-    if(!$rootScope.isAccountPresent){
-        $location.path('/signUp/')
-      console.log('hey');
-    }
-    
+        $('.loader').show()
+        if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($('#uname').val()))){
+                
+            return  $("#email_mismatch").show()
+        }
+        $http({
+            method: 'GET',
+            url: 'https://uttarbangasambad.in/api/users/getuser.php?email='+$('#uname').val()
+          }).then(function successCallback(response) {
+              if(response.status==200){
+                $rootScope.isAccountPresent=true
+                $location.path('/subscription/')
+                $('.loader').hide()
+              }
+           
+            }, function errorCallback(response) {
+              $location.path('/signUp/')
+              
+            });
+
     if($rootScope.isAccountPresent && !$rootScope.isSubscritonpresent ){
         $location.path('/subscription/')
         console.log('hi')
@@ -155,24 +214,6 @@ app.controller('signInController',function ($scope,$rootScope,$location,$http){
     
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-//google API
-
-
-// gapi.load('auth2', function(){
-//     gapi.auth2.init()
-// })
-
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
